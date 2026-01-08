f4a0e3dda282bb3f51af0f54cd39a2def94854ac
dhryu <ryudohyeon2@gmail.com>
2026-01-08 15:11:26 +0900

    UUID 등 타입 불일치 수정

--- voca_crm_api/src/main/java/com/vocacrm/api/dto/request/ReservationStatusUpdateRequest.java
+++ voca_crm_api/src/main/java/com/vocacrm/api/dto/request/ReservationStatusUpdateRequest.java
@@ -2,12 +2,11 @@ package com.vocacrm.api.dto.request;
 
 import com.vocacrm.api.model.Reservation.ReservationStatus;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.UUID;
 
 /**
  * 예약 상태 변경 요청 DTO
@@ -21,5 +20,6 @@ public class ReservationStatusUpdateRequest {
     @NotNull(message = "변경할 상태는 필수입니다")
     private ReservationStatus status;
 
-    private UUID updatedBy;
+    @Size(max = 36, message = "수정자 ID 형식이 올바르지 않습니다")
+    private String updatedBy;
 }
